a9cb54ed81c892a9718ad8d4b8b8371ffea95700
Rob Timmer <rob@robtimmer.com>
2016-06-03 15:37:01 +0200

    Removed redis server

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # Install dependencies
-apt-add-repository -y ppa:chris-lea/redis-server
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-apt-get -y install nodejs pwgen redis-server git vim
+apt-get -y install nodejs pwgen git vim
 apt-get clean
 
 # Setup log rotation to not spend up entire storage on logs
